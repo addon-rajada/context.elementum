ff9b11f0cf5684bd0ee9faea450706c05d008af6
Ícaro Maicon <46580318+icarok99@users.noreply.github.com>
2024-02-10 23:08:56 -0300

    Update context_play.py

--- context_play.py
+++ context_play.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 
-from plugin import doPlay
+from plugin import doPlayDownload
 
 if __name__ == '__main__':
-    doPlay()
+    doPlayDownload("play", False)
