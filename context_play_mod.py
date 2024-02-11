26d6c1ae3d2d733c74603cb84a0e32d3009893f9
Ícaro Maicon <46580318+icarok99@users.noreply.github.com>
2024-02-10 23:09:55 -0300

    Update context_play_mod.py

--- context_play_mod.py
+++ context_play_mod.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 
-from plugin import doPlayMod
+from plugin import doPlayDownloadMod
 
 if __name__ == '__main__':
-    doPlayMod()
+    doPlayDownloadMod("play", False)
